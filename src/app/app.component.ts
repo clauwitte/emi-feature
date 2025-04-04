b2865d5d64397908a78b9ee5c35f48293b575d08
Claudia.Cuadros <claudia.cuadros@rabobank.nl>
2025-04-04 19:26:32 +0200

    Import new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormComponent} from './form/form.component';
+import {MatToolbar} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormComponent, MatToolbar],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
